23abc5b2d7887e9c572d9a1bf2f685a0f7189b57
sebaszwenger <sebaszwenger@gmail.com>
2021-10-07 13:35:23 -0300

    incorporation and use style from Formulario.module.css

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,19 @@
+import styles from "./Formulario.module.css";
+
 const Formulario = () => {
   return (
-    <div className="row">
+    <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
         <form>
-          <h2>Encuentra Noticias por Categoría</h2>
+          <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+          <div className="input-field col s12">
+            <input
+              type="submit"
+              className={`${styles.btn_block} btn-large amber darken-2`}
+              value="Buscar"
+            />
+          </div>
         </form>
       </div>
     </div>
