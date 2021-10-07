c163ff6785f05443afd205271a2ed106271f0034
sebaszwenger <sebaszwenger@gmail.com>
2021-10-07 11:26:18 -0300

    new component Formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,13 @@
+const Formulario = () => {
+  return (
+    <div className="row">
+      <div className="col s12 m8 offset-m2">
+        <form>
+          <h2>Encuentra Noticias por Categoría</h2>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Formulario;
