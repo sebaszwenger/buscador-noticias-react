ce11a1db47cdbf7c0b99be04ec1f0b2e4595b4d4
sebaszwenger <sebaszwenger@gmail.com>
2021-10-07 21:29:56 -0300

    ending custom hook useSelect

--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -2,14 +2,22 @@ import { useState } from "react";
 
 const useSelect = (stateInicial, opciones) => {
   //Satet del custom hook
-  const [state, setState] = useState("");
+  const [state, setState] = useState(stateInicial);
 
-  const SelectNoticias = () => {
-    <select className="browse-default">
-      <option value="">Seleccione</option>
-    </select>;
-  };
-  return [state, SelectNoticias];
+  const SelectNoticias = () => (
+    <select
+      onChange={(e) => setState(e.target.value)}
+      value={state}
+      className="browser-default"
+    >
+      {opciones.map((opcion) => (
+        <option key={opcion.value} value={opcion.value}>
+          {opcion.label}
+        </option>
+      ))}
+    </select>
+  );
+  return [state, SelectNoticias, setState];
 };
 
 export default useSelect;
