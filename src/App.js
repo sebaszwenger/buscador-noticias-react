a2825fb63c3b44019c2eb6d50b5d937960877fbc
sebaszwenger <sebaszwenger@gmail.com>
2021-10-08 19:53:23 -0300

    add components categoria and noticias. add useEffect for categoria. call and save news from API.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
+import { useState, useEffect } from "react";
 
 function App() {
+  //definir la categoria y noticia
+  const [categoria, setCategoria] = useState("");
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=e88509129e5447158caedd02bc83b820`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      setNoticias(noticias.articles);
+    };
+
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <>
       <Header titulo="Buscador de Noticias" />
 
       <div className="container white">
-        <Formulario />
+        <Formulario setCategoria={setCategoria} />
       </div>
     </>
   );
