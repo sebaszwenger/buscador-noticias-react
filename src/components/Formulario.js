36bc40a13dca33291bd4fab45604503b3f7ce0fb
sebaszwenger <sebaszwenger@gmail.com>
2021-10-08 19:54:54 -0300

    add function buscarNoticias and call it on submit

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import styles from "./Formulario.module.css";
 import useSelect from "../hooks/useSelect";
 
-const Formulario = () => {
+const Formulario = ({ setCategoria }) => {
   //creandop categorias de busqueda según API
   const OPCIONES = [
     { value: "general", label: "General" },
@@ -16,10 +16,15 @@ const Formulario = () => {
   //Utilizamos custom hook
   const [categoria, SelectNoticias] = useSelect("general", OPCIONES);
 
+  const buscarNoticias = (e) => {
+    e.preventDefault();
+    setCategoria(categoria);
+  };
+
   return (
     <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
-        <form>
+        <form onSubmit={buscarNoticias}>
           <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
 
           <SelectNoticias />
